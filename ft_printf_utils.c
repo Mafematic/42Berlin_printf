f06f2b95733e969b7762688051704ce0a4dfd701
Friedrich Szendzielorz <fszendzi@c1b1c1.42berlin.de>
2023-06-11 19:10:38 +0200

    Amended hex size

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -137,7 +137,7 @@ int ft_putnbr_unsigned(unsigned int n)
 int ft_puthex(unsigned long long int n)
 {
 	char hex_digits[] = "0123456789abcdef";
-	char hex[9]; 
+	char hex[16]; 
 
 	int i = 0;
 	int remainder;
@@ -194,10 +194,18 @@ int ft_puthex_large(unsigned long long int n)
 
 int ft_putptr(void *ptr)
 {
-	if (ptr == NULL)
+	// Linux
+	/*if (ptr == NULL)
     {
         write(1, "(nil)", 5);
         return 5;
+    }
+	*/
+	// Mac
+	if (ptr == NULL)
+    {
+		write(1, "0x0", 3);
+        return 3;
     }
 	else
 	{
